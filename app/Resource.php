497c3eccaa1b0fc5d7103fa64159ac720a32c452
Zay Yar Min Htike <ZayYarMinHtike>
2019-09-14 23:16:19 +0630

    added auth,schema,model and controller

---
+++ app/Resource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    public function scopeSearch($query, $q) {
+        return $query->where('title','LIKE','%'.$q.'%')
+        ->orWhere('name','LIKE','%'.$q.'%')
+        ->orWhere('batch','LIKE','%'.$q.'%')
+        ->orWhere('mba/emba','LIKE','%'.$q.'%')
+        ->orWhere('topic','LIKE','%'.$q.'%')
+        ->orWhere('year','LIKE','%'.$q.'%')
+        ->orWhere('company','LIKE','%'.$q.'%')
+        ->orWhere('supervisor','LIKE','%'.$q.'%')
+        ->orWhere('abstract','LIKE','%'.$q.'%');
+    }   
+}
